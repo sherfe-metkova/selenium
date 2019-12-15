1712d4c76c7d9d6e0336b2b8c0214d42a0a4724d
sherfe-metkova <54353888+sherfe-metkova@users.noreply.github.com>
2019-12-15 17:05:16 +0200

    Add files via upload

--- selenium/src/test/java/Shop.java
+++ selenium/src/test/java/Shop.java
@@ -3,33 +3,111 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shop {
-    private static String extension = "@gmail.com";
-    private static String password = "CactussF&1334555555";
+    List<String> cartItems = new ArrayList<String>();
+    List<String> cartItemsPrice = new ArrayList<String>();
 
     @Test
-    public void shop() {
+    public void addItems() throws InterruptedException {
         WebDriver driver = common.getDriver();
         LoginForm.registerUser(driver, LoginForm.generateEmail());
         WebDriverWait wait = new WebDriverWait(driver, 10);
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#menu-item-40"))).click();
+        //click Shop
+        wait.until(ExpectedConditions.elementToBeClickable
+                (By.cssSelector("#menu-item-40 > a"))).click();
 
+        //scroll down
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         jsExecutor.executeScript("window.scrollBy(0,400)");
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#content > ul > li:nth-child(2) > .button"))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#content > ul > li:nth-child(3) > .button"))).click();
+        //add items to bag
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector
+                ("#content > ul > li:nth-child(2) > .button"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector
+                ("#content > ul > li:nth-child(3) > .button"))).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#wpmenucartli"))).click();
+        //check if bag number of items is updated
+        boolean isQuantityUpdated = false;
+        for (int i = 0; i < 20; i++) {
+            if (driver.findElements(By.className("cartcontents")).size() > 0) {
+                if (driver.findElement(By.className("cartcontents")).getText().
+                        equalsIgnoreCase("2 items")) {
+                    isQuantityUpdated = true;
+                    break;
+                }
+            }
+            Thread.sleep(500);
+        }
+        Assert.assertTrue("Cart text is not updated for the given 10 seconds", isQuantityUpdated);
 
-        driver.findElements(By.cssSelector("#page-34 > tbody > tr.cart_item")).size();
+        //click cart items
+        driver.findElement(By.id("wpmenucartli")).click();
 
-        Assert.assertEquals("Functional Programming in JS product is not added to Basket", "Functional Programming in JS", driver.findElement(By.cssSelector("#page-34 > div > div.woocommerce > form > table > tbody > tr:nth-child(2) > td.product-name > a")).getText());
-        Assert.assertEquals("HTML5 Forms product is not added to Basket", "HTML5 Forms", driver.findElement(By.cssSelector("#page-34 > div > div.woocommerce > form > table > tbody > tr:nth-child(1) > td.product-name > a")).getText());
+        wait.until(ExpectedConditions.presenceOfElementLocated
+                (By.cssSelector("#page-34 > div > div > form > table > tbody > tr.cart_item")));
 
+        //check that 2 items is shown
+        Assert.assertEquals("Items count in the cart is not the expected one", 2, driver.findElements(By.cssSelector
+                ("#page-34 > div > div > form > table > tbody > tr.cart_item")).size());
+
+        //create list containing items in cart
+        List<WebElement> itemsList = driver.findElements(By.cssSelector
+                ("#page-34 > div > div > form > table > tbody > tr.cart_item"));
+        cartItems.add("Functional Programming in JS");
+        cartItems.add("HTML5 Forms");
+        checkItems(cartItems, itemsList);
+
+        cartItemsPrice.add("250.00");
+        cartItemsPrice.add("280.00");
+        checkItemsPrice(cartItemsPrice, itemsList);
+
+        driver.quit();
     }
+
+    private void checkItems(List<String> cartItems, List<WebElement> itemsList) {
+        for (String itemName : cartItems) {
+            Assert.assertTrue(String.format("Item with name %s is not added to the cart", itemName),
+                    doesCollectionContainItem(itemName, itemsList));
+        }
+    }
+
+    private void checkItemsPrice(List<String> cartItemsPrice, List<WebElement> itemList){
+        for(String itemPrice : cartItemsPrice){
+            Assert.assertTrue(String.format("Item with price %s is not added to the cart", itemPrice),
+                    doesCollectionContainPrice(itemPrice, itemList));
+        }
+    }
+
+    private boolean doesCollectionContainItem(String itemText, List<WebElement> itemsList) {
+        for (WebElement element : itemsList) {
+            if (element.findElement(By.cssSelector(".product-name > a")).
+                    getText().equalsIgnoreCase(itemText)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean doesCollectionContainPrice(String itemPrice, List<WebElement> itemList){
+        for( WebElement element : itemList){
+            if(element.findElement(By.cssSelector(".product-price > span.woocommerce-Price-amount")).getText().equalsIgnoreCase(itemPrice)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //basket total check
+    //check other tabs on page
+    //update basket
+    //
 }
